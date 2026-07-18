964a051af85611c3946214607283ee68502bf968
Dhruv Ramaswamy <98780533+dhruvRamaswamy@users.noreply.github.com>
2026-07-18 12:33:18 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,21 @@
+#include <array>
+const int NUM_LETTERS = 26;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        std::array<int, NUM_LETTERS> scores = {0};
+        for(std::size_t i = 0; i < s.length(); i++) {
+            scores[int(s[i]) - 'a'] += 1;
+            scores[int(t[i]) - 'a'] -= 1;
+        }
+        for(int i = 0; i < NUM_LETTERS; i++) {
+            if(scores[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
