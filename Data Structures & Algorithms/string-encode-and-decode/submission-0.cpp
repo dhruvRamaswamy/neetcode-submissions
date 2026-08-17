d527d48ee64c61d997567096001758d5f2fd4891
Dhruv Ramaswamy <98780533+dhruvRamaswamy@users.noreply.github.com>
2026-08-17 19:02:47 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string retString = "";
+        for (const string& str : strs) {
+            // String bug (weird pointer arithmetic), must do toString
+            retString += std::to_string(str.size()) + "#" + str;
+        }
+        std::cout << retString;
+        return retString;
+    }
+
+    vector<string> decode(string s) {
+        //Get number of strings there are
+        
+        vector<string> ret;
+        size_t i = 0;
+        while(i < s.size()) {
+            size_t delimiterPosition = s.find("#", i);
+            std::cout << delimiterPosition;
+            // Get the number
+            int numChars = std::stoi(s.substr(i, delimiterPosition));
+            string word = s.substr(delimiterPosition + 1, numChars);
+            ret.push_back(word);
+            // Push it beyond delimiter plus the length of the word. 
+            // This is to acount for the top condition
+            // Also we don't have to keep a running track of the delimiter
+            i = delimiterPosition + 1 + numChars;
+
+        }
+        
+        
+        return ret;
+    }
+};
